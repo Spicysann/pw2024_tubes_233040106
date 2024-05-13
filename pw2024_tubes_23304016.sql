4aa9930067df63030937770340e899da35de8af8
Spycisann <sandyalfaizal49@gmail.com>
2024-05-13 21:56:57 +0700

    first commit

---
+++ pw2024_tubes_23304016.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 13, 2024 at 02:53 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw2024_tubes_23304016`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artis`
+--
+
+CREATE TABLE `artis` (
+  `id_artis` int NOT NULL,
+  `nama_artis` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `musik`
+--
+
+CREATE TABLE `musik` (
+  `id_musik` int NOT NULL,
+  `nama_musik` varchar(255) NOT NULL,
+  `genre_musik` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artis`
+--
+ALTER TABLE `artis`
+  ADD PRIMARY KEY (`id_artis`);
+
+--
+-- Indexes for table `musik`
+--
+ALTER TABLE `musik`
+  ADD PRIMARY KEY (`id_musik`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artis`
+--
+ALTER TABLE `artis`
+  MODIFY `id_artis` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `musik`
+--
+ALTER TABLE `musik`
+  MODIFY `id_musik` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `musik`
+--
+ALTER TABLE `musik`
+  ADD CONSTRAINT `musik_ibfk_1` FOREIGN KEY (`id_musik`) REFERENCES `artis` (`id_artis`) ON DELETE CASCADE ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
